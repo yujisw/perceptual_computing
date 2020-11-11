7ff1ff54edb2262f3a88dfd0ca4bbd102f3aef69
yujisw <yuji.1277@gmail.com>
2020-11-11 19:42:31 +0900

    add python file

---
+++ train.py
@@ -0,0 +1,132 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+import cv2
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import albumentations as albu
+from albumentations import pytorch as AT
+import segmentation_models_pytorch as smp
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+from dataset import CloudDataset
+import utils
+from loss import BCEDiceLoss
+from trainer import TrainEpoch, ValidEpoch
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("Using", DEVICE)
+
+print("preparing data...")
+
+path = './dataset/'
+train = pd.read_csv(f'{path}/train.csv')
+sub = pd.read_csv(f'{path}/sample_submission.csv')
+
+n_train = len(os.listdir(f'{path}/train_images'))
+n_test = len(os.listdir(f'{path}/test_images'))
+print(f'There are {n_train} images in train dataset')
+print(f'There are {n_test} images in test dataset')
+
+train['label'] = train['Image_Label'].apply(lambda x: x.split('_')[1])
+train['im_id'] = train['Image_Label'].apply(lambda x: x.split('_')[0])
+
+sub['label'] = sub['Image_Label'].apply(lambda x: x.split('_')[1])
+sub['im_id'] = sub['Image_Label'].apply(lambda x: x.split('_')[0])
+
+id_mask_count = train.loc[train['EncodedPixels'].isnull() == False, 'Image_Label'].apply(lambda x: x.split('_')[0]).value_counts().\
+reset_index().rename(columns={'index': 'img_id', 'Image_Label': 'count'})
+train_ids, valid_ids = train_test_split(id_mask_count['img_id'].values, random_state=42, stratify=id_mask_count['count'], test_size=0.1)
+test_ids = sub['Image_Label'].apply(lambda x: x.split('_')[0]).drop_duplicates().values
+
+print("creating preprocessing module...")
+
+ENCODER = 'resnet50'
+ENCODER_WEIGHTS = 'imagenet'
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+
+print("creating data loader...")
+
+num_workers = 0
+bs = 16
+
+train_dataset = CloudDataset(path = path, df=train, datatype='train', img_ids=train_ids, transforms = utils.get_training_augmentation(), preprocessing = utils.get_preprocessing(preprocessing_fn))
+valid_dataset = CloudDataset(path = path, df=train, datatype='valid', img_ids=valid_ids, transforms = utils.get_validation_augmentation(), preprocessing = utils.get_preprocessing(preprocessing_fn))
+
+train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True, num_workers=num_workers)
+valid_loader = DataLoader(valid_dataset, batch_size=bs, shuffle=False, num_workers=num_workers)
+
+loaders = {
+    "train": train_loader,
+    "valid": valid_loader
+}
+
+print("setting for training...")
+
+ACTIVATION = None
+model = smp.Unet(
+    encoder_name=ENCODER, 
+    encoder_weights=ENCODER_WEIGHTS, 
+    classes=4, 
+    activation=ACTIVATION,
+)
+
+num_epochs = 3
+logdir = "./logs/segmentation"
+
+# model, criterion, optimizer
+optimizer = torch.optim.Adam([
+    {'params': model.decoder.parameters(), 'lr': 1e-2}, 
+    {'params': model.encoder.parameters(), 'lr': 1e-3},  
+])
+scheduler = ReduceLROnPlateau(optimizer, factor=0.15, patience=2)
+loss = BCEDiceLoss()
+loss.__name__ = "BCEDiceLoss" # TODOこれを入れないとなんかエラー起きる
+metrics = [
+    smp.utils.metrics.IoU(threshold=0.5),
+]
+
+train_epoch = TrainEpoch(
+    model, 
+    loss=loss, 
+    metrics=metrics, 
+    optimizer=optimizer,
+    device=DEVICE,
+    verbose=True,
+)
+
+valid_epoch = ValidEpoch(
+    model, 
+    loss=loss, 
+    metrics=metrics, 
+    device=DEVICE,
+    verbose=True,
+)
+
+print("start training!")
+
+max_score = 0
+
+for i in range(0, num_epochs):
+    
+    print('\nEpoch: {}'.format(i))
+    train_logs = train_epoch.run(train_loader)
+    valid_logs = valid_epoch.run(valid_loader)
+    
+    # do something (save model, change lr, etc.)
+    if max_score < valid_logs['iou_score']:
+        max_score = valid_logs['iou_score']
+        torch.save(model, './best_model.pth')
+        print('Model saved!')
+        
+    if i % 1 == 0:
+        optimizer.param_groups[0]['lr'] = 1e-5
+        print('Decrease decoder learning rate to 1e-5!')
+
+print("training ends.")
+print('max_score:', max_score)
---
+++ utils.py
@@ -0,0 +1,86 @@
+import albumentations as albu
+import pandas as pd
+import numpy as np
+
+def rle_decode(mask_rle: str = '', shape: tuple = (1400, 2100)):
+    '''
+    Decode rle encoded mask.
+    
+    :param mask_rle: run-length as string formatted (start length)
+    :param shape: (height, width) of array to return 
+    Returns numpy array, 1 - mask, 0 - background
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape, order='F')
+
+def get_training_augmentation():
+    train_transform = [
+
+        albu.HorizontalFlip(p=0.5),
+        albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=0.5, border_mode=0),
+        albu.GridDistortion(p=0.5),
+        albu.OpticalDistortion(p=0.5, distort_limit=2, shift_limit=0.5),
+        albu.Resize(320, 640)
+    ]
+    return albu.Compose(train_transform)
+
+
+def get_validation_augmentation():
+    """Add paddings to make image shape divisible by 32"""
+    test_transform = [
+        albu.Resize(320, 640)
+    ]
+    return albu.Compose(test_transform)
+
+def get_preprocessing(preprocessing_fn):
+    """Construct preprocessing transform
+    
+    Args:
+        preprocessing_fn (callbale): data normalization function 
+            (can be specific for each pretrained neural network)
+    Return:
+        transform: albumentations.Compose
+    
+    """
+    
+    _transform = [
+        albu.Lambda(image=preprocessing_fn),
+        albu.Lambda(image=to_tensor, mask=to_tensor),
+    ]
+    return albu.Compose(_transform)
+
+def to_tensor(x, **kwargs):
+    """
+    Convert image or mask.
+    """
+    return x.transpose(2, 0, 1).astype('float32')
+
+def get_img(x, folder: str='train_images'):
+    """
+    Return image based on image name and folder.
+    """
+    data_folder = f"{path}/{folder}"
+    image_path = os.path.join(data_folder, x)
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+def make_mask(df: pd.DataFrame, image_name: str='img.jpg', shape: tuple = (1400, 2100)):
+    """
+    Create mask based on df, image name and shape.
+    """
+    encoded_masks = df.loc[df['im_id'] == image_name, 'EncodedPixels']
+    masks = np.zeros((shape[0], shape[1], 4), dtype=np.float32)
+
+    for idx, label in enumerate(encoded_masks.values):
+        if label is not np.nan:
+            mask = rle_decode(label)
+            masks[:, :, idx] = mask
+            
+    return masks
